69215a3a3360db4d03222780610c487bb04f8f73
2005934 <oh2976@gmail.com>
2023-07-13 14:36:06 +0900

    기부 프로젝트 구현 완료

--- app/src/main/java/com/example/greeningapp/ShoppingMainActivity.java
+++ app/src/main/java/com/example/greeningapp/ShoppingMainActivity.java
@@ -31,6 +31,7 @@ public class ShoppingMainActivity extends AppCompatActivity {
 
 
     private Button btnCart;
+    private Button btnDonation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +79,17 @@ public class ShoppingMainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ShoppingMainActivity.this, CartActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnDonation = (Button) findViewById(R.id.btnDonation);
+        btnDonation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ShoppingMainActivity.this, DonationMainActivity.class);
+                startActivity(intent);
+
             }
         });
     }
